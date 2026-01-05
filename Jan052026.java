112b249e26eab9e566efb0657ebd2b2b71bc7e37
Mohit Yadav <63849336+mohityada@users.noreply.github.com>
2026-01-05 21:16:14 +0530

    Jan052026
    Jan052026

--- Jan052026.java
+++ Jan052026.java
@@ -2,21 +2,19 @@ class Jan052026 {
     public long maxMatrixSum(int[][] a) {
         int n = a.length;
 
-        int sum = 0;
-        int count_neg = 0, max_neg = -1000000;
+        long sum = 0;
+        int count_neg = 0, min_elem = 100000000;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(a[i][j] < 0){
                     count_neg++;
-                    max_neg = Math.max(max_neg, a[i][j]);
                 }
-                sum += Math.abs(a[i][j]);
-                if(a[i][j] == 0) max_neg = 0;
+                int element = Math.abs(a[i][j]);
+                min_elem = Math.min(min_elem, element);
+                sum += element;                
             }
         }
-
-        if(count_neg%2 == 1) sum += 2*max_neg;
-
+        if(count_neg%2 == 1) sum -= 2*min_elem;
         return sum;
     }
 }
